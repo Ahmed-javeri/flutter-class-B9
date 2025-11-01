6f000a400aabbb197b0f5df7e7a9a47a5099a8ba
Ahmed-javeri <ahmed.javerri@gmail.com>
2025-11-01 18:29:04 +0500

    First commit: Dart List Assignment

---
+++ listAssignmentweek4.dart
@@ -0,0 +1,56 @@
+void main(){
+print("\t**Task1**");
+  List<String> Names=[
+    "Ahmed","Ali","Maaz","Irfan","Hadi","Bari"
+  ];
+  print(Names);
+print("\t**Task2**");
+  List<String> Days=[
+    "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+  ];
+  print(Days.last);
+
+print("\t**Task3**");
+  List aboutMe=[
+    "Ahmed","Flutter B9",16277,"A+","84%"
+  ];
+  print(aboutMe);
+
+print("\t**Task4**");
+List<int> Numbers=[
+  11,14,16,155,15,74,83,72,28,24
+];
+Numbers.sort();
+print('smallest Numbers ${Numbers.first}'" & Largest Number ${Numbers.last}");
+
+print("\t**Task5**");
+List<int> greatestVal=[
+  11,14,16,155,15,74,83,72,28,24
+];
+greatestVal.sort();
+print(greatestVal.last);
+
+print("\t**Task6**");
+  List<String> gadgets=[
+    "Mobile","Laptop","Airbuds","ipad","ipod","Smartwatch","Television"  ];
+  List<String> reversed= gadgets.reversed.toList();
+  print(gadgets);
+  print(reversed);
+
+print("***SelfTask***");
+List<int> selfTask=[
+  11,14,16,155,15,74,83,72,28,24
+];
+selfTask.sort();
+print(selfTask);
+selfTask.add(11);
+print(selfTask);
+selfTask.remove(28);
+print(selfTask);
+selfTask.removeAt(2);
+print(selfTask);
+selfTask.removeLast();
+print(selfTask);
+selfTask.clear();
+print(selfTask);
+}
